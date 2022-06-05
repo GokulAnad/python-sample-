0196c98e6c36ba414d5447fa6d3e91083c0b3eb3
GokulAnad <fooddynatural@gmail.com>
2022-06-05 20:46:26 +0530

    python class object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Myclass:
+    year=2020
     def __init__(self,name,age,place):
         self.name=name
         self.age=age
@@ -6,15 +7,27 @@ class Myclass:
         print(self.name,self.age,self.place)
     def addage(self):
         self.age=self.age+1
-        print(self.age)
+
     def relocated(self,replace):
         self.place=replace
-        print(self.place)
+    def display(self):
+        print(self.name,self.age,self.place,Myclass.year)
+    @classmethod
+    def addyear(cls):
+        cls.year=cls.year+1
+    @staticmethod
+    def home():
+        print("welcome")
 
 
 
+Myclass.home()
 x=Myclass("manu",25,"palode")
+
+Myclass.addyear()
 x.addage()
 x.relocated("ndd")
+x.display()
+
 
 
